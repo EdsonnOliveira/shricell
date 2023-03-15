e0cb5c92176f038f6d6a2160952b25c18e6bc0e7
Edson.Talent <edson.talent@fleetcor.com.br>
2023-03-15 09:36:33 -0300

    Changed CSS

--- src/atomic/constants/colors.tsx
+++ src/atomic/constants/colors.tsx
@@ -6,5 +6,8 @@ export const white = '#FFF';
 export const black = '#0F0F0F';
 export const gray = '#696969';
 export const grey = '#F4F4F4';
+
 export const blue = '#132538';
 export const lightBlue = '#EBF6FF';
+export const green = '#04E36B';
+export const red = '#E31504';
