f94430967d0fbd5ef38dfdb38461fcbef788f87e
Edson.Talent <edson.talent@fleetcor.com.br>
2023-03-18 16:30:57 -0300

    New Styled Type: FlexWrap

--- src/atomic/constants/align.tsx
+++ src/atomic/constants/align.tsx
@@ -3,6 +3,7 @@ import { StyledProps } from "styled-components";
 export type FlexDirection = 'row' | 'column'
 export type JustifyContent = 'flex-start' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly' | 'center'
 export type AlignItems = 'flex-start' | 'flex-end' | 'center'
+export type FlexWrap = 'unset' | 'wrap' | 'wrap-reverse'
 
 export type Alignments = {
     flexDirection?: FlexDirection;
@@ -12,6 +13,7 @@ export type Alignments = {
     alignItems?: AlignItems;
     alignItems800?: AlignItems;
     gap?: string;
+    flexWrap: FlexWrap;
 }
 
 export type AlignmentsStyledProps = StyledProps<Alignments>
