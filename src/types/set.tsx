e51305465ff5e94801f4b4980782cc5847cfa5dd
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-04 10:13:11 -0300

    New type: SetBoolean

--- src/types/set.tsx
+++ src/types/set.tsx
@@ -1,3 +1,4 @@
 export type SetText = (value: string) => void;
 export type SetFile = (file: FileList | null) => void;
+export type SetBoolean = (state: boolean) => void;
 export type SetVoid = () => void;
