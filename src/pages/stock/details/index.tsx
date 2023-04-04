fd1dba7ab652b259373d705714d5384c4ddf20b7
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-03 21:33:50 -0300

    Set new endpoints

--- src/pages/stock/details/index.tsx
+++ src/pages/stock/details/index.tsx
@@ -1,5 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+
+import { OptionsType } from "@atomic/constants/select";
+
+import brands from "@api/stock/brands";
+import models from "@api/stock/models";
+import colors from "@api/stock/colors";
+import grades from "@api/stock/grades";
+import storages from "@api/stock/storages";
+import { BrandProps } from "@api/stock/brands/models";
+import { ModelsProps } from "@api/stock/models/models";
+import { ColorsProps } from "@api/stock/colors/models";
+import { GradesProps } from "@api/stock/grades/models";
+import { StoragesProps } from "@api/stock/storages/models";
 
 import View from "./view";
 
@@ -8,73 +21,113 @@ const StockDetails: React.FC = ({
     const router = useRouter().query
 
     const [brand, setBrand] = useState<string>('')
-
-    const brandsItems = [
-        {
-            label: 'teste',
-            value: 'teste'
-        },
-        {
-            label: 'teste2',
-            value: 'teste2'
-        },
-    ]
+    const [brandsItems, setBrandsItems] = useState<OptionsType[]>([{ label: 'Select the Brand', value: '-1' }])
 
     const [model, setModel] = useState<string>('')
-
-    const modelItems = [
-        {
-            label: 'teste',
-            value: 'teste'
-        },
-        {
-            label: 'teste2',
-            value: 'teste2'
-        },
-    ]
+    const [modelsItems, setModelsItems] = useState<OptionsType[]>([{ label: 'Select the Model', value: '-1' }])
 
     const [color, setColor] = useState<string>('')
-
-    const colorItems = [
-        {
-            label: 'teste',
-            value: 'teste'
-        },
-        {
-            label: 'teste2',
-            value: 'teste2'
-        },
-    ]
+    const [colorsItems, setColorsItems] = useState<OptionsType[]>([{ label: 'Select the Color', value: '-1' }])
 
     const [grade, setGrade] = useState<string>('')
-
-    const gradeItems = [
-        {
-            label: 'teste',
-            value: 'teste'
-        },
-        {
-            label: 'teste2',
-            value: 'teste2'
-        },
-    ]
+    const [gradesItems, setGradesItems] = useState<OptionsType[]>([{ label: 'Select the Grade', value: '-1' }])
 
     const [storage, setStorage] = useState<string>('')
-
-    const storageItems = [
-        {
-            label: 'teste',
-            value: 'teste'
-        },
-        {
-            label: 'teste2',
-            value: 'teste2'
-        },
-    ]
+    const [storagesItems, setStoragesItems] = useState<OptionsType[]>([{ label: 'Select the Storage', value: '-1' }])
 
     const [price, setPrice] = useState<string>('')
     const [cost, setCost] = useState<string>('')
     const [quantity, setQuantity] = useState<string>('')
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    const loadData = () => {
+        brands.listAll()
+        .then((data: BrandProps[]) => {
+            let options: OptionsType[] = [{ label: 'Select the Brand', value: '-1' }]
+            let array = data.map(item => (
+                {
+                    label: item.brand,
+                    value: String(item.brandId)
+                }
+            ))
+            setBrandsItems([...options, ...array])
+        })
+
+        grades.listAll()
+        .then((data: GradesProps[]) => {
+            let options: OptionsType[] = [{ label: 'Select the Grade', value: '-1' }]
+            let array = data.map(item => (
+                {
+                    label: item.gradeDescription,
+                    value: String(item.gradeId)
+                }
+            ))
+            setGradesItems([...options, ...array])
+        })
+
+        storages.listAll()
+        .then((data: StoragesProps[]) => {
+            let options: OptionsType[] = [{ label: 'Select the Storages', value: '-1' }]
+            let array = data.map(item => (
+                {
+                    label: item.storage,
+                    value: String(item.storageId)
+                }
+            ))
+            setStoragesItems([...options, ...array])
+        })
+    }
+
+    useEffect(() => {
+        loadModels()
+    }, [brand])
+
+    const loadModels = () => {
+        if (brand === '-1' || brand.length <= 0)
+            return
+
+        setModelsItems([{label: 'Loading...', value: '-1'}])
+        setModel('-1')
+        
+        models.listAll({ brandId: brand })
+        .then((data: ModelsProps[]) => {
+            let options: OptionsType[] = [{ label: 'Select the Model', value: '-1' }]
+            let array = data.map(item => (
+                {
+                    label: item.model,
+                    value: String(item.modelId)
+                }
+            ))
+            setModelsItems([...options, ...array])
+        })
+    }
+
+    useEffect(() => {
+        loadColors()
+    }, [model])
+
+    const loadColors = () => {
+        if (model === '-1' || model.length <= 0)
+            return
+
+        setColorsItems([{label: 'Loading...', value: '-1'}])
+        setColor('-1')
+        
+        colors.listAll({ brandId: brand, modelId: model })
+        .then((data: ColorsProps[]) => {
+            let options: OptionsType[] = [{ label: 'Select the Color', value: '-1' }]
+            let array = data.map(item => (
+                {
+                    label: item.color,
+                    value: String(item.colorId)
+                }
+            ))
+            setColorsItems([...options, ...array])
+        })
+    }
 
     return (
         <View
@@ -85,16 +138,16 @@ const StockDetails: React.FC = ({
             brandItems={brandsItems}
             brand={brand}
             setBrand={setBrand}
-            modelItems={modelItems}
+            modelItems={modelsItems}
             model={model}
             setModel={setModel}
-            colorItems={colorItems}
+            colorItems={colorsItems}
             color={color}
             setColor={setColor}
-            gradeItems={gradeItems}
+            gradeItems={gradesItems}
             grade={grade}
             setGrade={setGrade}
-            storageItems={storageItems}
+            storageItems={storagesItems}
             storage={storage}
             setStorage={setStorage}
             price={price}
--- src/pages/stock/index.tsx
+++ src/pages/stock/index.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 import { TR } from "@atomic/constants/table";
-import { green, red } from "@atomic/constants/colors";
+import { green } from "@atomic/constants/colors";
 import { HeaderItemsPreview } from "@atomic/constants/header";
 
+import { currency } from "@constants/formats";
+
+import devices from "@api/stock/devices";
+import { DevicesProps } from "@api/stock/devices/models";
+
 import View from "./view";
-import { useRouter } from "next/router";
 
 const itemsPreview: HeaderItemsPreview[] = [
     {
@@ -24,358 +29,71 @@ const Stock: React.FC = ({
 }) => {
     const router = useRouter();
 
-    const latestSales: TR[] = [
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/stock/details',
-                    query: {
-                        isEdit: true,
-                        stockName: 'iPhone 14 Pro Max'
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '4',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: red
-                        }
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Blue',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-        {
-            td: [
-                    {
-                        description: 'iPhone 14',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '128GB',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Purple',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '13',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: '$ 799',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ]
-        },
-    ]
+    const [data, setData] = useState<TR[]>([])
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    const loadData = () => {
+        devices.listAll()
+        .then((data: DevicesProps[]) => {
+            let array = data.map(item => (
+                {
+                    td: [
+                            {
+                                description: item.model,
+                                textAlign: 'left',
+                                textWeight: '500',
+                                type: 'text'
+                            },
+                            {
+                                description: item.storage,
+                                textAlign: 'center',
+                                textWeight: '300',
+                                type: 'text'
+                            },
+                            {
+                                description: item.color,
+                                textAlign: 'center',
+                                textWeight: '300',
+                                type: 'text'
+                            },
+                            {
+                                description: item.quantityStock || '0',
+                                textAlign: 'center',
+                                textWeight: '500',
+                                type: 'text'
+                            },
+                            {
+                                description: `$ ${currency(Number(item.salePrice || 0), 2, 3, '.', ',')}`,
+                                textAlign: 'center',
+                                textWeight: '500',
+                                type: {
+                                    color: 'fontWhite',
+                                    bgColor: green
+                                }
+                            },
+                    ],
+                    onClick: () => {
+                        router.push({
+                            pathname: '/stock/details',
+                            query: {
+                                isEdit: true,
+                                stockName: item.model
+                            }}
+                        )
+                    }
+                }
+            ))
+            setData(array)
+        })
+    }
 
     return (
         <View
             router={router}
-            latestSales={latestSales}
+            data={data}
             itemsPreview={itemsPreview}
         />
     )
