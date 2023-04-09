2f55859f4a492411792b9c50a5a22236f4482668
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-09 16:49:56 -0300

    New type: SetIndex

--- src/types/set.tsx
+++ src/types/set.tsx
@@ -2,3 +2,4 @@ export type SetText = (value: string) => void;
 export type SetFile = (file: FileList | null) => void;
 export type SetBoolean = (state: boolean) => void;
 export type SetVoid = () => void;
+export type SetIndex = (index: number) => void;
