afeb7df2fbf08962c0162f85cb839c4eb370c2e3
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-09 14:34:59 -0300

    New borderRadius type

--- src/atomic/constants/button.tsx
+++ src/atomic/constants/button.tsx
@@ -1,6 +1,7 @@
 import IndexProps from '@atomic/atoms/button/models'
 
 export const borderRadius = '14px'
+export const borderRadiusSmall = '10px'
 export const transition = '.4s'
 
 export type ButtonType = IndexProps;
