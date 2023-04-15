2d0e9afd2ccd3583f818009da18bd27a99853cc7
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-14 22:28:57 -0300

    Added Redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,16 @@ import '@atomic/constants/text.css'
 
 import type { AppProps } from 'next/app'
 import { Rubik } from '@next/font/google'
+import { storeWrapper } from '@redux/store'
 
 const rubik = Rubik({ subsets: ['latin'] })
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
     <main className={rubik.className}>
       <Component {...pageProps} />
     </main>
   )
 }
+
+export default storeWrapper.withRedux(App)
