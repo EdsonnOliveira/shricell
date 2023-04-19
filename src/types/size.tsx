4a7c0d04fcccc756a7669161de162b4b5d31f2aa
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-19 18:08:20 -0300

    News props in size type: maxWidth and maxHeight

--- src/types/size.tsx
+++ src/types/size.tsx
@@ -1,5 +1,7 @@
 export type Size = {
     width?: string | number | undefined;
+    maxWidth?: string | number | undefined;
     height?: string | number | undefined;
+    maxHeight?: string | number | undefined;
     flex?: string | number | undefined;
 }
