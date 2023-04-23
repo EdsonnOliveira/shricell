6bdcbd343bb457a42a03425ed675aa4d6e7f8ebb
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-23 16:09:19 -0300

    Customer page adjustments

--- src/pages/admin/customers/index.tsx
+++ src/pages/admin/customers/index.tsx
@@ -1,302 +1,94 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { CustomersTypes } from "@redux/reducers/customers/models";
 
 import { TR } from "@atomic/constants/table";
 import { green, primary, red } from "@atomic/constants/colors";
 import { HeaderItemsPreview } from "@atomic/constants/header";
 
+import customer from "@api/customer";
+import { CustomerProps } from "@api/customer/models";
+
 import View from "./view";
-import { useRouter } from "next/router";
+import { IndexProps } from "./models";
 
-const itemsPreview: HeaderItemsPreview[] = [
-    {
-        icon: '',
-        title: 'Total',
-        value: '74'
-    },
-]
-
-const Customers: React.FC = ({
+const Customers: React.FC<IndexProps> = ({
+    setDataCustomer
 }) => {
     const router = useRouter();
 
-    const data: TR[] = [
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Approved',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Rejected',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: red
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Approved',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Approved',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Approved',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: green
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-        {
-            td: [
-                    {
-                        description: 'iTech',
-                        textAlign: 'left',
-                        textWeight: '500',
-                        type: 'text'
-                    },
-                    {
-                        description: 'email@email.com',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: '+55 (00) 00000-000',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Brazil, DF, Brasília',
-                        textAlign: 'center',
-                        textWeight: '300',
-                        type: 'text'
-                    },
-                    {
-                        description: 'Pending',
-                        textAlign: 'center',
-                        textWeight: '500',
-                        type: {
-                            color: 'fontWhite',
-                            bgColor: primary
-                        }
-                    },
-            ],
-            onClick: () => {
-                router.push({
-                    pathname: '/customers/details',
-                    query: {
-                        isEdit: true,
-                        idCustomer: 1
-                    }}
-                )
-            }
-        },
-    ]
+    const [itemsPreview, setItemsPreview] = useState<HeaderItemsPreview[]>([])
+    const [data, setData] = useState<TR[]>([])
+
+    useEffect(() => {
+        loadData()
+    }, [])
+
+    const loadData = () => {
+        customer.listAll()
+        .then((data: CustomerProps[]) => {
+            setItemsPreview([
+                {
+                    icon: '',
+                    title: 'Customers',
+                    value: String(data.length)
+                },
+            ])
+
+            let array = data.map((item, index) => (
+                {
+                    td: [
+                            {
+                                description: 'item.name',
+                                textAlign: 'left',
+                                textWeight: '500',
+                                type: 'text'
+                            },
+                            {
+                                description: item.email,
+                                textAlign: 'center',
+                                textWeight: '300',
+                                type: 'text'
+                            },
+                            {
+                                description: item.phone,
+                                textAlign: 'center',
+                                textWeight: '300',
+                                type: 'text'
+                            },
+                            {
+                                description: item.companyAddress,
+                                textAlign: 'center',
+                                textWeight: '300',
+                                type: 'text'
+                            },
+                            {
+                                description: item.status,
+                                textAlign: 'center',
+                                textWeight: '500',
+                                type: {
+                                    color: 'fontWhite',
+                                    bgColor: green
+                                }
+                            },
+                    ],
+                    onClick: () => {
+                        setDataCustomer(data[index])
+                        router.push({
+                            pathname: '/admin/customers/details',
+                            query: {
+                                isEdit: true,
+                            }}
+                        )
+                    }
+                }
+            ))
+            setData(array)
+        })
+    }
 
     const [modalAdd, setModalAdd] = useState<boolean>(false)
 
@@ -311,4 +103,10 @@ const Customers: React.FC = ({
     )
 }
 
-export default Customers;
+const mapStateToProps = ({}) => ({})
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    setDataCustomer: (data: CustomersTypes['data']) => dispatch({ type: 'SET_CUSTOMER_DATA', payload: { data } }),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Customers);
--- src/pages/admin/customers/models.tsx
+++ src/pages/admin/customers/models.tsx
@@ -1,6 +1,11 @@
 import { HeaderItemsPreview } from "@atomic/constants/header";
 import { NextRouter } from "next/router";
 import { TR } from "@atomic/constants/table";
+import { CustomersTypes } from "@redux/reducers/customers/models";
+
+export interface IndexProps {
+    setDataCustomer: (data: CustomersTypes['data']) => void
+}
 
 export interface ViewProps {
     router: NextRouter;
