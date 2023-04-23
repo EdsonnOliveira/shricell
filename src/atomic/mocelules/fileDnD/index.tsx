37c771255822ee2549746e67172e0840b3f8b976
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-23 16:08:52 -0300

    New prop in fileDnD: accept

--- src/atomic/mocelules/fileDnD/index.tsx
+++ src/atomic/mocelules/fileDnD/index.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import BoxCommon from "@atomic/atoms/boxCommon";
 import { IndexProps } from "./models";
 import { Main, FileInput } from "./style";
 
 const FileDnD: React.FC<IndexProps> = ({
     file,
     setFile,
+    accept,
     mt,
     ml,
     mr,
@@ -22,16 +24,22 @@ const FileDnD: React.FC<IndexProps> = ({
     }
 
     return (
-        <Main drag={dragEnter} mt={mt} ml={ml} mr={mr}mb={mb}>
+        <Main drag={dragEnter || file?.length > 0} mt={mt} ml={ml} mr={mr}mb={mb}>
             {
                 file && file?.length > 0
                 ? <h4>{ file[0].name }</h4>
                 : dragEnter
                     ? <h4 className='fontPrimary'>Drop file!</h4>
-                    : <h4>Drop file here, or <b className='fontPrimary fontW400'>browse</b></h4>
+                    : (
+                        <BoxCommon alignItems='center' gap='5px'>
+                            <h4>Drop file here, or <b className='fontPrimary fontW400'>browse</b></h4>
+                            { accept && <h6 className='fontW500'>Suports: { accept }</h6> }
+                        </BoxCommon>
+                    )
             }
             <FileInput
                 type='file'
+                accept={accept}
                 onDragOver={onDragOver}
                 onDrop={onDrop}
                 onDragEnter={() => setDragEnter(true)}
--- src/atomic/mocelules/fileDnD/models.tsx
+++ src/atomic/mocelules/fileDnD/models.tsx
@@ -4,6 +4,7 @@ import { Margins } from "../../constants/spacing";
 export interface IndexProps extends Margins {
     file: FileList | null
     setFile: (file: FileList | null) => void;
+    accept?: string;
 }
 
 export type IndexStyledProps = StyledProps<{
