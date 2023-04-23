0d3e79b1c0657d4434a1b07831a7632ddcb2501d
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-23 16:10:22 -0300

    New endpoint: bank

---
+++ src/services/api/bank/index.tsx
@@ -0,0 +1,30 @@
+import api from '@api/index'
+import { BankProps } from './models'
+
+const listAll = () => {
+    return new Promise(async (resolve, reject) => {
+        await api.get('bank/banks.php')
+        .then(response => {
+            let res:BankProps[] = response.data.banks
+            let array:BankProps[] = []
+
+            for (let i = 0; i < res.length; i++ ) {
+                let json: BankProps = {
+                    bankId: res[i].bankId,
+                    bank: res[i].bank,
+                    accountNumber: res[i].accountNumber,
+                    routingNumber: res[i].routingNumber,
+                    businessName: res[i].businessName,
+                    businessAddress: res[i].businessAddress
+                }
+                array.push(json)
+            }
+            resolve(array)
+        })
+        .catch((response) => reject(response))
+    })
+}
+
+export default {
+    listAll,
+}
---
+++ src/services/api/bank/models.tsx
@@ -0,0 +1,8 @@
+export interface BankProps {
+    bankId: number,
+    bank: string,
+    accountNumber: string | number,
+    routingNumber: string | number,
+    businessName: string,
+    businessAddress: string
+}
