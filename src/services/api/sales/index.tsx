847767bae2f45731fd397ae690b17972aea8f0e9
Edson.Talent <edson.talent@fleetcor.com.br>
2023-04-25 22:36:09 -0300

    New endpoint: sales

--- src/services/api/sales/index.tsx
+++ src/services/api/sales/index.tsx
@@ -1,6 +1,57 @@
 import api from '@api/index'
 import { IndexType, SaleProps } from './models'
 
+const listAll = () => {
+    return new Promise(async (resolve, reject) => {
+        await api.post('sales/sales.php')
+        .then(response => {
+            let res:SaleProps[] = response.data.sales
+            let array:SaleProps[] = []
+            for (let i = 0; i < res.length; i++ ) {
+                let json: SaleProps = {
+                    saleId: res[i].saleId,
+                    customerId: res[i].customerId,
+                    companyName: res[i].companyName,
+                    saleValue: res[i].saleValue,
+                    dateTimeInsert: res[i].dateTimeInsert,
+                    hash: res[i].hash,
+                    saleCost: res[i].saleCost,
+                    paymentReceipt: res[i].paymentReceipt
+                }
+                array.push(json)
+            }
+            resolve(array || [])
+        })
+        .catch((response) => reject(response))
+    })
+}
+
+const listAllCustomer = ({ customerId }: IndexType) => {
+    return new Promise(async (resolve, reject) => {
+        await api.post('sales/sales-customer.php', { customerId })
+        .then(response => {
+            let res:SaleProps[] = response?.data?.sales
+            let array:SaleProps[] = []
+
+            for (let i = 0; i < res?.length; i++ ) {
+                let json: SaleProps = {
+                    saleId: res[i].saleId,
+                    customerId: res[i].customerId,
+                    companyName: res[i].companyName,
+                    saleValue: res[i].saleValue,
+                    dateTimeInsert: res[i].dateTimeInsert,
+                    hash: res[i].hash,
+                    saleCost: res[i].saleCost,
+                    paymentReceipt: res[i].paymentReceipt
+                }
+                array.push(json)
+            }
+            resolve(array)
+        })
+        .catch((response) => reject(response))
+    })
+}
+
 const insert = ({ customerId, paymentReceipt }: IndexType) => {
     return new Promise(async (resolve, reject) => {
         await api.post('sales/new-sale.php', { customerId, paymentReceipt }, {
@@ -17,5 +68,7 @@ const insert = ({ customerId, paymentReceipt }: IndexType) => {
 }
 
 export default {
+    listAll,
+    listAllCustomer,
     insert
 }
--- src/services/api/sales/models.tsx
+++ src/services/api/sales/models.tsx
@@ -1,8 +1,16 @@
 export type IndexType = {
-    customerId: string | number;
-    paymentReceipt: FileList | null;
+    customerId?: string | number;
+    paymentReceipt?: FileList | null;
 }
 
 export interface SaleProps {
     message?: string;
+    saleId?: string | number;
+    customerId?: string | number;
+    companyName?: string | number;
+    saleValue?: string | number;
+    dateTimeInsert?: string | number;
+    hash?: string | number;
+    saleCost?: string | number;
+    paymentReceipt?: string | number;
 }
