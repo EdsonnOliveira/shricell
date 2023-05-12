0ec105357b639dda5f4607393a15051f379007c7
Edson.Talent <edson.talent@fleetcor.com.br>
2023-05-12 19:13:07 -0300

    New date constant

--- src/constants/date.tsx
+++ src/constants/date.tsx
@@ -1,8 +1,19 @@
 export function getDateCurrent() {
     const date = new Date();
-    let month = String(date.getMonth()).padStart(2, '0')
+    let month = String(date.getMonth()+1).padStart(2, '0')
     let day = String(date.getDate()).padStart(2, '0')
     let year = String(date.getFullYear())
     
     return `${month}/${day}/${year}`
+}
+
+export function getFirstLastDay() {
+    const date = new Date();
+    let month = String(date.getMonth()+1).padStart(2, '0')
+    let year = String(date.getFullYear())
+    
+    return {
+        first: `${month}/01/${year}`,
+        last: `${month}/31/${year}`
+    }
 }
