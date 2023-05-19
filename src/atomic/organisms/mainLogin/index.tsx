5da22aecc70f03d7a1b78b2fdb346c6dc6794364
Edson.Talent <edson.talent@fleetcor.com.br>
2023-05-19 01:48:51 -0300

    Test 12

--- src/atomic/organisms/mainLogin/index.tsx
+++ src/atomic/organisms/mainLogin/index.tsx
@@ -6,7 +6,14 @@ import BoxCommon from "@atomic/atoms/boxCommon";
 import Button from "@atomic/atoms/button";
 
 import { IndexProps } from "./models";
-import { Background, Container, Logo, Main, Test } from "./style";
+// import { Background, Container, Logo, Main, Test } from "./style";
+import styled from "styled-components"
+
+export const Test = styled.div`
+    width: 100px;
+    height: 100px;
+    background-color: red;
+`
 
 const MainLogin: React.FC<IndexProps> = ({
     children,
