b8ba0dd1cdde8e0c0037adfc8022248245484d28
Edson.Talent <edson.talent@fleetcor.com.br>
2023-05-19 01:25:01 -0300

    Test 5

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Home: React.FC<IndexProps> = ({
   const router = useRouter()
   const [email, setEmail] = useState<string>('')
   const [password, setPassword] = useState<string>('')
-  const [steps, setSteps] = useState<Steps>('login')
+  const [steps, setSteps] = useState<Steps>('register')
 
   const [dunsNumber, setDunsNumber] = useState<string>('')
   const [federalTax, setFederalTax] = useState<string>('')
